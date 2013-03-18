b8f6c6ac50fe535396152d2718c9bceaf4141a4c
Nickolas Nikolic <subclass@gmail.com>
2013-03-18 13:53:48 -0500

    Threw in a few images and display for imageView requirement

--- com.nickolasnikolic.facilities/controllers/widget.js
+++ com.nickolasnikolic.facilities/controllers/widget.js
@@ -1,9 +1,26 @@
-var args = arguments[0] || {};
+var args = arguments[ 0 ] || {};
 
-var offset = 0;
-
-function doInit(args) {
-	$.test.text = "Whom's your daddy?";
+function myInit( args ){
+	//get the passed params
+	if( args ){
+		//set the imageView to those values
+		//send out notice if worked or not
+		if( myIndoorMapsRoute( args.location ) ){
+			Ti.App.fireEvent( 'indoorMap', true );
+		}else{
+			Ti.App.fireEvent( 'indoorMap', false );
+		}
+	}
 }
 
-exports.init    = doInit;
+function myIndoorMapsRoute( imageToDisplay ){
+	try{
+		var path = '/maps/' + imageToDisplay + '.jpg';
+		$.image = WPATH( path );
+		return true;
+	}catch( e ){
+		return false;
+	}
+}
+
+exports.init = myInit;
